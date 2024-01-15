1601f8c098ab0a106f8bb9e1eaef5cc02375a86a
Luke Nielsen (N4 Studios) <73626301+darthluke04@users.noreply.github.com>
2024-01-14 21:48:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,63 @@
-var words = require('an-array-of-english-words'); // Array of english words
+/*------------ Modules and Dictionaries ------------*/
+const words = require('an-array-of-english-words');	// * Array of english words
+const fs = require('fs');				// * File System Module
+const path = require("path");				// * Path Module
+const Cryptr = require('cryptr');			// * Decrypt module
+const bf = require('bruteforce'); 			// * Bruteforce Module (Determaine Key)
+const two = ['b', 'q'];					// * Two chars (For Testing)
+const num = [0,1,2,3,4,5,6,7,8,9];			// * All possible numbers
+const alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ' ', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '`', '-', '=', '{', '}', '[', ']', ':', '\'', ';', '\"', ',', '.', '/', '<', '>', '?', '\\', '|']; // The chararacter dictionary
 
-const fs = require('fs'); // File System Module
+/*------------ CONSTANTS - DO CHANGE ------------*/
+// Main
+const folder = 'example/'; 				//   ["example/"] * Which directory to scan
+const finder = "flag{"; 				//   ["flag{"]    * Beginning of string to scan E.g, flag{
+const finderEnd = "}"; 					//   ["}"]        * End of string to scan E.g, }
+const decryption = true;				// ! [true]       * Decrypts flag if in file (true or false)
+const bruteforceKeys = true;				// ! [true]       * Guesses the key by using brute force if true. WARNING: Takes longer to read if true (true or false).
+const includeMax = 50; 					// ! [50]         * Max amount of words read. - WARNING: Takes longer to read if higher
+// Security
+var cryptKey = "myTotallySecretKey";			//   ["Key"]      * The key for decryption. [Same as the one that the owner used to encrypt]
+const bruteLengthMax = 010;				// ! [0005]       * Bruteforce key length max. - WARNING: Takes longer if higher.
+const bruteLengthMin = 0001;				//   [true]       * Bruteforce key length min. (GREATER THAN 0)
+const brutePrintMain = true;				//   [false]      * Print all of bruteforce info.
+const brutePrintPerc = true;				//   [false]      * Print bruteforce Percentage. (bar + Advanced Percentage)
+const brutePrintTest = false;				//   [false]      * Print the current key being tested.
+const brutePrintFlag = true;				//   [false]      * Print the current flag that is being decrypted.
+const brutePrintPrints = false;				//   [false]      * Print the amount of trys / prints it has bruteforced for the current flag.
+const brutePrintOther = false;				//   [false]      * Print other bruteforce info.
+const brutePrintTime = true;				//   [false]      * Print time between each key.
+// Security Dictionaries
+const bruteDictionary = (alph); 			//   [chars]      * The dictionary that the brute force will use to find the key.
+// Print Settings
+const barLength = 10;					//   [10]         * Print Length for Percentage Bar of the bruteforce
+const devInfo = true; 					//   [true]       * Include data read in file (true or false)
+const flagDevInfo = true; 				//   [true]       * disable developer info even if flag found (true or false)
+const includeInfo = false; 				// ! [false]      * Include possible words in file. - WARNING: Takes longer if true. (true or false)
+const dataMax = 500; 					// ! [500]        * Max amount of data printed. - WARNING: Takes longer to read if higher
 
-const path = require("path"); // Path Module
 
-// Constants
-const folder = 'example/'; 				// Which directory to scan
-const finder = "flag{"; 	// Beginning of string to scan E.g, flag{
-const finderEnd = "}"; 				// End of string to scan E.g, }
-
-// WARNING: Takes longer to read if higher
-const includeMax = 50; 				// Max amount of words read
-const dataMax = 500; 					// Max amount of data printed
-
+/*------------ VARIABLES, DO NOT CHANGE ------------*/
 // Data
-var flagAnswer = ""; 					// Current flag answer
-var numberFile = 0; 					// Current number of files scanned
-var totalmb = 0; 							// Total Megabytes scanned
-var estTotalmb = 28.842; 			// Total Megabytes in directory
+var flagAnswer = ""; 					// * Current flag answer
+var numberFile = 0; 					// * Current number of files scanned
+var totalmb = 0; 					// * Total Megabytes scanned
+var estTotalmb = 28.842; 				// * Total Megabytes in directory
+// Security
+var cryptr = new Cryptr(cryptKey); 			// * Decrypt string module
+var bruteLength = bruteLengthMax;			// * current Brute Force key length
+var bruteStartTime = Date.now();
+var bruteEndTime = Date.now();
+var bruteTime = 0;
+var bruteKPS = 0;
+var bruteTimeLeftH = 0;
+var bruteTimeLeftM = 0;
+var bruteTimeLeftS = 0;
+var bruteTimeInitial = 0;
 
-// Developer Info
-var devInfo = false; 					// Include data read in file (true or false)
-var flagDevInfo = true; 			// disable developer info even if flag found (true or false)
-
-// WARNING: Takes longer to read if true
-var includeInfo = false; 			// Include possible words in file (true or false)
-
-// NOT IMPLEMENTED
-var checkForHashes = false;		// Checks for hashes in file (true or false)
 
 start();
-
 // Funciton to run
 function start() {
 	var flags = ""; // String that holds all flags
@@ -49,7 +76,7 @@ function start() {
 					return;
 				}
 
-				// Increase
+				// Increase file index
 				numberFile += 1;
 				totalmb += (((new TextEncoder().encode(data)).length) * 0.000001);
 
@@ -100,10 +127,96 @@ function start() {
 					var flag = data.substring(data.toLowerCase().indexOf(finder), (data.substring(data.toLowerCase().indexOf(finder)).indexOf(finderEnd)) + data.toLowerCase().indexOf(finder) + 1);
 					flagAnswer = flag;
 					flags += (flag + " : " + file + "\n");
+					var settingKey = true;
 					if (flagDevInfo == true) {
 						console.log("\nFlag Index: " + data.toLowerCase().indexOf(finder));
 						console.log("Flag Length: " + flag.length);
-						console.log("\x1B[36mFlag: " + flags + "\u001b[32m");
+						console.log("\x1B[36mFlag: " + flags + "");
+						//console.log(words);
+						var prints = 0;
+//						var bruteLengthMax = bruteLength;
+						var maxPrints = Number(Math.pow(bruteDictionary.length, bruteLengthMax)).toLocaleString('fullwide', {useGrouping: false});
+//						var maxPrints = Number((Math.pow(bruteLengthMax, bruteDictionary.length))).toLocaleString('fullwide', {useGrouping: false});
+						var barEq = "";
+						var barSp = "";
+						function printDecrypt(key) {
+							prints += 1;
+							barSp = "";
+							if(brutePrintOther == true || brutePrintMain == true) {console.log("\x1b[93mPress \x1b[31mCTRL + C\x1b[93m to quit.\u001b[32m");}
+							if(brutePrintPrints == true || brutePrintMain == true) {console.log("\x1b[31mPrints                 : " + prints + " of " + maxPrints + " \x1b[93m("  + ((prints / maxPrints) * 100).toFixed(3) +  "%)\u001b[32m");}
+							function printBar() {
+								eqLength = barLength * (prints / maxPrints);
+								//console.log(eqLength);
+								spLength = barLength - (((eqLength.toFixed(0)) * barLength)) / 100;
+								//console.log(spLength);
+								for(let i = 0; barEq.length < eqLength; i++) {
+									barEq += "\x1b[93m=";
+								}
+							}
+							function printBruteTime() {
+								bruteEndTime = Date.now();
+								bruteTime = (bruteEndTime - bruteStartTime);
+								bruteKPS = Number((bruteTime/ 1000) * 60).toLocaleString('fullwide', {useGrouping: false});
+								bruteTimeLeftH = Number(((bruteTime * maxPrints) / 600) / 6000).toLocaleString('fullwide', {useGrouping: false});
+								bruteTimeLeftM = Number(bruteTimeLeftH * 60).toLocaleString('fullwide', {useGrouping: false});
+								bruteTimeLeftS = Number(bruteTimeLeftM * 60).toLocaleString('fullwide', {useGrouping: false});
+								bruteTimeElapsed = Number(((((bruteEndTime - bruteTimeInitial) / 60) / 6) / 3).toLocaleString('fullwide', {useGrouping: false}));
+								if(brutePrintTime == true || brutePrintMain == true) {
+									console.log("\x1b[93mKeys Per Second        : \x1b[36m" + bruteKPS + " KPS");
+									console.log("\x1b[93mEstimated Hours Left   : \x1b[36m" + bruteTimeLeftH + " Hours");
+									console.log("\x1b[93mEstimated Minutes Left : \x1b[36m" + bruteTimeLeftM + " Minutes");
+									console.log("\x1b[93mEstimated Seconds Left : \x1b[36m" + bruteTimeLeftS + " Seconds ");
+									console.log("\x1b[93mSeconds Elapsed        : " + bruteTimeElapsed.toFixed(0) + " Seconds");
+								}
+							}
+							try {
+								if(brutePrintFlag == true || brutePrintMain == true) {console.log("\x1b[36mFlag(Decrypted)         : \x1B[93m\x1B[36m" + finder + cryptr.decrypt(data.substring((data.toLowerCase().indexOf(finder)) + finder.length, (data.substring(data.toLowerCase().indexOf(finder)).indexOf(finderEnd)) + data.toLowerCase().indexOf(finder)) + finderEnd  + "\u001b[32m"));}
+								printBar();
+								printBruteTime();
+								if(brutePrintPerc == true || brutePrintMain == true) {console.log("\x1b[93mPercentage             : \x1b[36m"  + ((prints / maxPrints) * 100).toFixed(20) +  "%\u001b[32m");}
+								return(false);
+							} catch (error) {
+								//console.log(error);
+								if(brutePrintFlag == true || brutePrintMain == true) {console.log("\x1b[36mFlag(Original)         : \x1B[93m\x1B[36m" + flag + "\u001b[32m");}
+								printBar();
+								printBruteTime();
+								if(brutePrintPerc == true || brutePrintMain == true) {console.log("\x1b[93mPercentage             : \x1b[36m"  + ((prints / maxPrints) * 100).toFixed(20) +  "%\u001b[32m");}
+								return(true);
+							}
+						}
+						function setKey(key) {
+							if(settingKey == true) {
+								if(key == "") {key = " "};
+								cryptKey = key;
+								if(brutePrintTest == true || brutePrintMain == true) {console.log("\x1b[93mTesting Key (" + key.length + " Chars)  : \x1b[36m" + key + "\n\u001b[32m");}
+								cryptr = new Cryptr(cryptKey);
+								settingKey = printDecrypt(key);
+								for(let i = 0; barSp.length < spLength; i++) {
+									barSp += "\x1b[36m-";
+								}
+								bruteStartTime = Date.now();
+							} else {
+								return(false);
+							}
+						}
+						if (decryption == true && settingKey == true) {
+							bruteLength = bruteLengthMax;
+							bruteTimeInitial = Date.now();
+							for(bruteLength = bruteLengthMax; bruteLength <= bruteLengthMax && bruteLength >= bruteLengthMin; bruteLength--) {
+								bf({
+									len: bruteLength,
+									chars: bruteDictionary,
+									step: setKey,
+									//end: console.log("\x1b[36mFlag(Decrypted) : \x1B[93m\x1B[36m" + finder + cryptr.decrypt(data.substring((data.toLowerCase().indexOf(finder)) + finder.length, (data.substring(data.toLowerCase().indexOf(finder)).indexOf(finderEnd)) + data.toLowerCase().indexOf(finder))) + finderEnd  + "\u001b[32m")
+								});
+								try {
+									if(brutePrintMain == true || brutePrintMain == true) {console.log("\x1b[36mFlag(Decrypted) : \x1B[93m\x1B[36m" + finder + cryptr.decrypt(data.substring((data.toLowerCase().indexOf(finder)) + finder.length, (data.substring(data.toLowerCase().indexOf(finder)).indexOf(finderEnd)) + data.toLowerCase().indexOf(finder))) + finderEnd  + "\u001b[32m");}
+								} catch (error) {
+									//console.log(error);
+									if(brutePrintMain == true || brutePrintMain == true) {console.log("\x1b[36mFlag(Original) : \x1B[93m\x1B[36m" + flag + "\u001b[32m");}
+								}
+							}
+						}
 					}
 				}
 
@@ -113,7 +226,7 @@ function start() {
 
 				// Print flag(s) found
 				if (flagAnswer != "" && flags != "") {
-					console.log("\x1B[35mFLAGs: \n" + flags + "\033[0m")
+					console.log("\x1B[35mFLAGs: \n" + flags + "\u001b[32m");
 				}
 			});
 		}
